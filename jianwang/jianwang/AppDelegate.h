14a3f9a9cc20ce01acc9f381338bc62e97c1f357
admin003 <770447719@qq.com>
2016-10-09 16:11:18 +0800

    健网

---
+++ jianwang/jianwang/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  jianwang
+//
+//  Created by admin003 on 2016/10/9.
+//  Copyright © 2016年 cj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ jianwang/jianwang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jianwang
+//
+//  Created by admin003 on 2016/10/9.
+//  Copyright © 2016年 cj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
